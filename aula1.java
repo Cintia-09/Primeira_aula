36e75df2e6084218e789da576846cbb1bc8b5459
Francisca Cíntia de Sousa Barros <cintiabarros@alu.ufc.br>
2021-05-17 14:26:02 -0300

    Create aula1.java

---
+++ aula1.java
@@ -0,0 +1,28 @@
+
+/**
+ * Escreva uma descrição da classe projeto aqui.
+ * 
+ * @author (Cíntia) 
+ * @version (um número da versão ou uma data)
+ */
+public class projeto
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String oi;
+
+    /**
+     * Construtor para objetos da classe projeto
+     */
+    public projeto(String n)
+    {
+        // inicializa variáveis de instância
+        n = oi;
+        System.out.print(oi); 
+    }
+
+    
+    {
+        // escreva seu código aqui
+         System.out.print(oi); 
+    }
+}
