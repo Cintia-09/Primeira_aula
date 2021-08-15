d890bf45efe639dc15b6772efad64475ca3f06c2
Francisca Cíntia de Sousa Barros <cintiabarros@alu.ufc.br>
2021-08-15 17:48:21 -0300

    Add files via upload

---
+++ ProjetoFinal/__SHELL6.java
@@ -0,0 +1,8 @@
+package ProjetoFinal;
+public class __SHELL6 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+ProjetoFinal.Main.main(__bluej_param0);
+
+}}
